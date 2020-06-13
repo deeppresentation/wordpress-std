e611664ad332ed0f8e167ecbfd91076552d26aea
green-g <tomas.groulik@gmail.com>
2020-06-13 17:56:13 +0200

    Generic library commit 1592063771962

--- src/Acf.php
+++ src/Acf.php
@@ -21,7 +21,7 @@ class Acf
         if ($postId)
         {
             $table = get_field($fieldId, $postId, false);
-           /* if (is_string($tableIntroSteps))
+           /* if (is_string($tableIntroSteps)) asss
             {
                 $tableIntroSteps = json_decode($tableIntroSteps, true);
             }*/
