9ca77c28ebf53b79914a569ff7d8b99517ca3f48
green-g <tomas.groulik@gmail.com>
2021-02-28 15:40:34 +0100

    Generic library commit 1614523230802

--- src/AdminNotice.php
+++ src/AdminNotice.php
@@ -32,15 +32,20 @@ class AdminNotice
         );
     }
 
+    public static function get_ask_for_rating_text(int $activeWeeks, string $productName, string $textDomain, $add_greeting_at_the_end = true){
+        $plugin_name = '<strong>' . $productName . '</strong>';
+        $weeksText = sprintf(esc_html(_n("%d week", "%d weeks", $activeWeeks, $textDomain)), $activeWeeks);
+        $text = sprintf(__("Awesome, you've been using %s for more then %s.<br>Nice rating helps us grow, so we can <strong>serve you better</strong> via our support and develop <strong>new cool features</strong> into this free product.<br>We would really appreciate Your help. It takes just one minute. Thank You:)<br>", $textDomain), $plugin_name, $weeksText);
+        if ($add_greeting_at_the_end){
+            $text .= __("<strong>Your DeepPresentation Team</strong>", $textDomain); 
+        }
+        return $text;
+    }
+
     public static function render_ask_for_rating_notice(int $activeWeeks, string $productName, string $linkForRating, string $textDomain, string $imgUrl = null, string $imgAlt = 'Dp Intro Tour Rating Image')
     {
-        $plugin_name = '<strong>' . $productName . '</strong>';
-        $weeksText = sprintf(esc_html(_n("%d week", "%d weeks", $activeWeeks,$textDomain)), $activeWeeks);
-        $text = sprintf(__("Awesome, you've been using %s for more then %s. We would be happy and we would really appreciate Your help via rating system at WordPress.org. Nice rating helps us with further development and support of this free version and it takes just one minute. Thank You:)<br>"
-            . "<strong>Your DeepPresentation Team</strong>", $textDomain), $plugin_name, $weeksText);
-
-
-            AdminNotice::render_raw_notice(\array_filter([
+        $text = self::get_ask_for_rating_text($activeWeeks, $productName, $textDomain);
+        AdminNotice::render_raw_notice(\array_filter([
                 ($imgUrl) ? new Element('img', 'img', ['src' => $imgUrl, 'alt' => $imgAlt, 'class' => 'dp-intro-tour-a4r-5star-img'], null, null, false) : null,
                 new Element('p', 'text', null, $text),
                 new Element('a', 'link', ['href' => $linkForRating, 'target' => '_blank', 'id'=>'a4r-link-OK'], __('OK, you deserved it', $textDomain)),
