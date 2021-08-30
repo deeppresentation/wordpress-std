fb1e9a793594a4352c8ad6ff4564c30fd2490d6b
Tomáš Groulík <tomas.groulik@gmail.com>
2021-08-30 22:00:05 +0200

    Generic library commit 1630353602954

--- src/AdminNotice.php
+++ src/AdminNotice.php
@@ -4,58 +4,119 @@ use DP\Std\Html\Element;
 use DP\Std\Core\Arr;
 
 
-class AdminNotice
-{
-    public static function render_raw_notice($htmlContent, string $type = 'success', string $noticeBEMClass = 'dp-notice', ?string $id = null, string $noticeExtraClass = '')
-    {
-        $clases = [
-            "notice notice-${type}", 
-            "is-dismissible",
-            $noticeExtraClass
-        ];
+class AdminNotice {
 
-        $notice = new Element('div', $noticeBEMClass, ['class' => Arr::as_string($clases, ' '), 'id'=>$id], $htmlContent);
-        $notice->render();
-    }
+	public static function render_raw_notice( $htmlContent, string $type = 'success', string $noticeBEMClass = 'dp-notice', ?string $id = null, string $noticeExtraClass = '' ) {
+		$clases = array(
+			"notice notice-${type}",
+			'is-dismissible',
+			$noticeExtraClass,
+		);
 
-    public static function render_notice(string $text, string $type = 'success', bool $hasButton = false, string $buttonLink = '', string $buttonClass = '', string $buttonText = '', bool $linkInNewTab = true, string $buttonSubText = '', string $noticeBEMClass = 'dp-notice', ?string $id = null, string $noticeExtraClass = '')
-    {
-        self::render_raw_notice([
-            new Element('p', 'text', null, $text),
-                (!$hasButton) ? null : new Element('a', 'button', [
-                    'class' => $buttonClass,
-                    'href' => $buttonLink,
-                    'target' => ($linkInNewTab) ? '_blank' : '',
-                ], [$buttonText, !empty($buttonSubText) ? new Element('span', 'sub-text', null, $buttonSubText) : null])
-            ], 
-            $type, $noticeBEMClass, $id, $noticeExtraClass
-        );
-    }
+		$notice = new Element(
+			'div',
+			$noticeBEMClass,
+			array(
+				'class' => Arr::as_string( $clases, ' ' ),
+				'id'    => $id,
+			),
+			$htmlContent
+		);
+		$notice->render();
+	}
 
-    public static function get_ask_for_rating_text(int $activeWeeks, string $productName, string $textDomain, $add_greeting_at_the_end = true){
-        $plugin_name = '<strong>' . $productName . '</strong>';
-        if ($activeWeeks > 16) $time_in_use_text = sprintf(esc_html(_n("%d month", "%d months", floor($activeWeeks / 4), $textDomain)), floor($activeWeeks / 4));
-        else $time_in_use_text = sprintf(esc_html(_n("%d week", "%d weeks", $activeWeeks, $textDomain)), $activeWeeks);
-        $text = sprintf(__("Amazing, you've been using the %s for over %s.<br>Nice rating helps us grow, so we can <strong>serve you better</strong> via our support and develop <strong>new cool features</strong> into this free product.<br>We really appreciate Your help. It takes just one minute. Thank You:)<br>", $textDomain), $plugin_name, $time_in_use_text);
-        if ($add_greeting_at_the_end){
-            $text .= __("<strong>Your DeepPresentation Team</strong>", $textDomain); 
-        }
-        return $text;
-    }
+	public static function render_notice( string $text, string $type = 'success', bool $hasButton = false, string $buttonLink = '', string $buttonClass = '', string $buttonText = '', bool $linkInNewTab = true, string $buttonSubText = '', string $noticeBEMClass = 'dp-notice', ?string $id = null, string $noticeExtraClass = '' ) {
+		self::render_raw_notice(
+			array(
+				new Element( 'p', 'text', null, $text ),
+				( ! $hasButton ) ? null : new Element(
+					'a',
+					'button',
+					array(
+						'class'  => $buttonClass,
+						'href'   => $buttonLink,
+						'target' => ( $linkInNewTab ) ? '_blank' : '',
+					),
+					array( $buttonText, ! empty( $buttonSubText ) ? new Element( 'span', 'sub-text', null, $buttonSubText ) : null )
+				),
+			),
+			$type,
+			$noticeBEMClass,
+			$id,
+			$noticeExtraClass
+		);
+	}
 
-    public static function render_ask_for_rating_notice(int $activeWeeks, string $productName, string $linkForRating, string $textDomain, string $notice_id = 'a4r-notice', string $imgUrl = null, string $imgAlt = '')
-    {
-        $text = self::get_ask_for_rating_text($activeWeeks, $productName, $textDomain);
-        AdminNotice::render_raw_notice(\array_filter([
-                ($imgUrl) ? new Element('img', 'img', ['src' => $imgUrl, 'alt' => $imgAlt, 'class' => 'dp-intro-tour-a4r-5star-img'], null, null, false) : null,
-                new Element('p', 'text', null, $text),
-                new Element('a', 'link', ['href' => $linkForRating, 'target' => '_blank', 'id'=>'a4r-link-OK', 'class' => 'button button-primary button-pro-promo'], __('OK, you deserved it', $textDomain)),
-                '<br>',
-                new Element('a', 'link', ['href' => '#', 'id'=>'a4r-link-already-did'], __('I already did', $textDomain), 'already-did'),
-                new Element('a', 'link', ['href' => '#', 'id'=>'a4r-link-no-good'], __('No, not good enough', $textDomain), 'no-good')
-            ]),
-            'success', 'dp-notice', $notice_id
-        );
-    }
+	public static function get_ask_for_rating_text( int $activeWeeks, string $productName, string $textDomain, $add_greeting_at_the_end = true ) {
+		$plugin_name = '<strong>' . $productName . '</strong>';
+		if ( $activeWeeks > 16 ) {
+			$time_in_use_text = sprintf( esc_html( _n( '%d month', '%d months', floor( $activeWeeks / 4 ), 'DP_TEXT_DOMAIN_TO_PREFIX' ) ), floor( $activeWeeks / 4 ) );
+		} else {
+			$time_in_use_text = sprintf( esc_html( _n( '%d week', '%d weeks', $activeWeeks, 'DP_TEXT_DOMAIN_TO_PREFIX' ) ), $activeWeeks );
+		}
+		$text = sprintf( __( "Amazing, you've been using the %1\$s for over %2\$s.<br>Nice rating helps us grow, so we can <strong>serve you better</strong> via our support and develop <strong>new cool features</strong> into this free product.<br>We really appreciate Your help. It takes just one minute. Thank You:)<br>", $textDomain ), $plugin_name, $time_in_use_text );
+		if ( $add_greeting_at_the_end ) {
+			$text .= __( '<strong>Your DeepPresentation Team</strong>', 'DP_TEXT_DOMAIN_TO_PREFIX' );
+		}
+		return $text;
+	}
+
+	public static function render_ask_for_rating_notice( int $activeWeeks, string $productName, string $linkForRating, string $textDomain, string $notice_id = 'a4r-notice', string $imgUrl = null, string $imgAlt = '' ) {
+		$text = self::get_ask_for_rating_text( $activeWeeks, $productName, $textDomain );
+		AdminNotice::render_raw_notice(
+			\array_filter(
+				array(
+					( $imgUrl ) ? new Element(
+						'img',
+						'img',
+						array(
+							'src'   => $imgUrl,
+							'alt'   => $imgAlt,
+							'class' => 'dp-intro-tour-a4r-5star-img',
+						),
+						null,
+						null,
+						false
+					) : null,
+					new Element( 'p', 'text', null, $text ),
+					new Element(
+						'a',
+						'link',
+						array(
+							'href'   => $linkForRating,
+							'target' => '_blank',
+							'id'     => 'a4r-link-OK',
+							'class'  => 'button button-primary button-pro-promo',
+						),
+						__( 'OK, you deserved it', $textDomain )
+					),
+					'<br>',
+					new Element(
+						'a',
+						'link',
+						array(
+							'href' => '#',
+							'id'   => 'a4r-link-already-did',
+						),
+						__( 'I already did', $textDomain ),
+						'already-did'
+					),
+					new Element(
+						'a',
+						'link',
+						array(
+							'href' => '#',
+							'id'   => 'a4r-link-no-good',
+						),
+						__( 'No, not good enough', $textDomain ),
+						'no-good'
+					),
+				)
+			),
+			'success',
+			'dp-notice',
+			$notice_id
+		);
+	}
 
 }
--- src/GoogleGeo.php
+++ src/GoogleGeo.php
@@ -3,57 +3,53 @@
 use DP\Std\Core\Arr;
 use DP\Std\Core;
 
-class GoogleGeo extends Core\GoogleGeo
-{
-    public function __construct($apiKey)
-    {
-        parent::__construct($apiKey);
-    }
+class GoogleGeo extends Core\GoogleGeo {
 
-    public static function get_data_from_wp_post(?int $postId = null)
-    {
-        if (!$postId) $postId = get_the_ID();
-        $data = self::get_initialized_data();
-        if ($postId)
-        { 
-            foreach ($data as $key => $val)
-            {
-                $data[$key] = get_field($key, $postId);
-            }
-        }
-        return $data;
-    }
+	public function __construct( $apiKey ) {
+		parent::__construct( $apiKey );
+	}
 
-    public static function clear_data_in_wp_post(?int $postId = null)
-    {
-        if (!$postId) $postId = get_the_ID();
-        if ($postId)
-        {
-            $data = self::get_initialized_data();
-            foreach ($data as $key => $val) 
-            {
-                WpStd::update_post_meta(
-                    $postId,
-                    $key,
-                    ''
-                );
-            }
-        }
-    }
+	public static function get_data_from_wp_post( ?int $postId = null ) {
+		if ( ! $postId ) {
+			$postId = get_the_ID();
+		}
+		$data = self::get_initialized_data();
+		if ( $postId ) {
+			foreach ( $data as $key => $val ) {
+				$data[ $key ] = get_field( $key, $postId );
+			}
+		}
+		return $data;
+	}
 
-    public static function set_data_to_wp_post(array $geoData, ?int $postId = null)
-    {
-        if (!$postId) $postId = get_the_ID();
-        if ($postId)
-        {
-            foreach ($geoData as $key => $val) 
-            {
-                WpStd::update_post_meta(
-                    $postId,
-                    $key,
-                    $val
-                );
-            }
-        }
-    }
+	public static function clear_data_in_wp_post( ?int $postId = null ) {
+		if ( ! $postId ) {
+			$postId = get_the_ID();
+		}
+		if ( $postId ) {
+			$data = self::get_initialized_data();
+			foreach ( $data as $key => $val ) {
+				WpStd::update_post_meta(
+					$postId,
+					$key,
+					''
+				);
+			}
+		}
+	}
+
+	public static function set_data_to_wp_post( array $geoData, ?int $postId = null ) {
+		if ( ! $postId ) {
+			$postId = get_the_ID();
+		}
+		if ( $postId ) {
+			foreach ( $geoData as $key => $val ) {
+				WpStd::update_post_meta(
+					$postId,
+					$key,
+					$val
+				);
+			}
+		}
+	}
 }
