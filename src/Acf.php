fc5da2705ac0d49bc1062362f272959e5548b072
Tomáš Groulík <tomas.groulik@gmail.com>
2022-01-03 20:37:50 +0100

    Generic library commit 1641238668438

--- src/Acf.php
+++ src/Acf.php
@@ -56,14 +56,14 @@ class Acf {
 	public static function decode_raw_to_assoc_array_of_columns( array $acfTableField, $useHeaderValues = false, &$labels = null ) {
 		if ( array_key_exists( 'h', $acfTableField ) && array_key_exists( 'b', $acfTableField ) ) {
 			if ( $useHeaderValues ) {
-				$labels = array();
+				$labels = [];
 				foreach ( $acfTableField['h'] as $hIdx => $headerItem ) {
 					$labels[] = Arr::sget( $headerItem, 'c', $hIdx );
 				}
 			}
-			$res = array();
+			$res = [];
 			foreach ( $acfTableField['b'] as $rIdx => $rowDataInput ) {
-				$rowData = array();
+				$rowData = [];
 				foreach ( $acfTableField['h'] as $hIdx => $headerItem ) {
 					$rowData[ Arr::sget( $headerItem, $useHeaderValues ? 'v' : 'c', $hIdx ) ] = wp_specialchars_decode( Arr::sget( $rowDataInput, $hIdx . '.c' ) );
 				}
@@ -111,8 +111,8 @@ class Acf {
 	public static function create_table_field_from_assoc_array_of_columns( array $dataTableColumns, bool $prependRowNameColumn = false, bool $isValueUsed = false, ?array $valOrLabelsKeys = null, ?string $version = null ) {
 		// asociative array of columns
 
-		$header   = array();
-		$dataRows = array();
+		$header   = [];
+		$dataRows = [];
 		if ( ! empty( $dataTableColumns ) ) {
 			$columnKeys = array_keys( $dataTableColumns );
 			if ( ! empty( $columnKeys ) ) {
@@ -120,38 +120,38 @@ class Acf {
 
 				// header
 				if ( $prependRowNameColumn ) {
-					$header[] = array( 'c' => '' );
+					$header[] = [ 'c' => '' ];
 				}
 				$i = 0;
 				foreach ( $columnKeys as $columnKey ) {
 					$headerVal  = Arr::sget( $valOrLabelsKeys, $i, '' );
-					$headerData = array(
+					$headerData = [
 						'c' => self::sanitize_arrays_fields( $isValueUsed ? $headerVal : $columnKey ),
 						'v' => self::sanitize_arrays_fields( $isValueUsed ? $columnKey : $headerVal ),
-					);
+					];
 					$header[]   = $headerData;
 					$i++;
 				}
 				// data rows
 				foreach ( $rowKeys as $rowKey ) {
-					$row = array();
+					$row = [];
 					if ( $prependRowNameColumn ) {
-						$row[] = array( 'c' => $rowKey );
+						$row[] = [ 'c' => $rowKey ];
 					}
 					foreach ( $dataTableColumns as $dataTableColumn ) {
-						$row[] = array( 'c' => self::sanitize_arrays_fields( $dataTableColumn[ $rowKey ] ) );
+						$row[] = [ 'c' => self::sanitize_arrays_fields( $dataTableColumn[ $rowKey ] ) ];
 					}
 					$dataRows[] = $row;
 				}
 			}
 		}
-		return array(
-			'acftf' => array( 'v' => $version ?? '1.3.10' ),
-			'p'     => array( 'o' => array( 'uh' => 1 ) ),
-			'c'     => array_fill( 0, count( $header ), array( 'p' => '' ) ),
+		return [
+			'acftf' => [ 'v' => $version ?? '1.3.10' ],
+			'p'     => [ 'o' => [ 'uh' => 1 ] ],
+			'c'     => array_fill( 0, count( $header ), [ 'p' => '' ] ),
 			'h'     => $header,
 			'b'     => $dataRows,
-		);
+		];
 	}
 
 	public static function table_encode_from_assoc_array_of_columns( array $dataTableColumns, bool $prependRowNameColumn = false, int $jsonEncodeBitMaskOption = 0, bool $isValueUsed = false, ?array $valOrLabelsKeys = null, ?string $version = null ) {
@@ -164,7 +164,7 @@ class Acf {
 			$postId = get_the_ID();
 		}
 		if ( $postId ) {
-			update_field( $relationShipFieldTag, array(), $postId );
+			update_field( $relationShipFieldTag, [], $postId );
 		}
 	}
 
@@ -184,7 +184,7 @@ class Acf {
 		if ( $field_cfg ) {
 			$type = Arr::get( $field_cfg, 'type', $def );
 			if ( $type === 'group' ) {
-				$res        = array();
+				$res        = [];
 				$sub_fields = Arr::get( $field_cfg, 'sub_fields', null );
 				if ( $sub_fields ) {
 					foreach ( $sub_fields as $sub_field ) {
@@ -202,6 +202,9 @@ class Acf {
 			} else {
 				$val = Arr::get( $field_cfg, 'default_value' );
 				if ( isset( $val ) ) {
+					if ( $type === 'select' && \is_array( $val ) && count( $val ) ) {
+						$val = $val[0];
+					}
 					if ( $formatValue ) {
 						$val = apply_filters( 'acf/format_value', $val, null, $field_cfg );
 					}
@@ -241,7 +244,7 @@ class Acf {
 		if ( $postId && $relatedPostId ) {
 			$currentRelationships = get_field( $relationShipFieldTag, $postId, false );
 			if ( empty( $currentRelationships ) ) {
-				$currentRelationships = array();
+				$currentRelationships = [];
 			}
 			array_push( $currentRelationships, $relatedPostId );
 			update_field( $relationShipFieldTag, array_unique( $currentRelationships ), $postId );
@@ -252,8 +255,8 @@ class Acf {
 		if ( ! $value ) {
 			return true;
 		}
-		$h      = Arr::get( $value, 'h', array() );
-		$b      = Arr::get( $value, 'b', array() );
+		$h      = Arr::get( $value, 'h', [] );
+		$b      = Arr::get( $value, 'b', [] );
 		$hEmpty = count( $h ) == 0 || ( count( $h ) == 1 && empty( Arr::sget( $h, '0.c' ) ) );
 		$bEmpty = count( $b ) == 0 || ( count( $b ) == 1 && ( count( $b[0] ) == 0 || count( $b[0] ) == 1 && empty( Arr::sget( $b, '0.0.c' ) ) ) );
 		return $hEmpty && $bEmpty;
